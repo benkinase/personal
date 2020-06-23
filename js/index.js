46af2660806da96a324e675f91237eff06f2047a
gbenimako@gmail.com <gbenimako@gmail.com>
2020-06-23 19:08:32 +0200

    animation on brand

--- js/index.js
+++ js/index.js
@@ -3,18 +3,23 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+
+const topLink = document.querySelector(".top-link");
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
+	const scrollHeight = window.pageYOffset;
+  if (scrollHeight> 80) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
   }
-});
+
+
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
+
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
